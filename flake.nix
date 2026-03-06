b7a4dc005e9b1f752e70653e2b5ed3734abe9c82
nertsal <nestralnertsal@gmail.com>
2026-03-07 00:35:38 +0300

    nix flake-ify

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    geng.url = "github:geng-engine/cargo-geng/e5ed1324056150c2768dfb239c0ee79244a11fc2";
+    geng.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { geng, nixpkgs, systems, self }:
+    let
+      pkgsFor = system: import nixpkgs { inherit system; };
+      forEachSystem = f: nixpkgs.lib.genAttrs (import systems) (system:
+        let
+          pkgs = pkgsFor system;
+        in
+        f { inherit system pkgs; });
+    in
+    {
+      devShells = forEachSystem ({ system, pkgs, ... }:
+        {
+          default = geng.lib.mkShell {
+            inherit system;
+            target.linux.enable = true;
+            target.web.enable = true;
+            packages = with pkgs; [
+              just
+              bacon
+              butler
+              sqlite
+              rlwrap
+              sqlx-cli
+            ];
+          };
+        });
+      formatter = forEachSystem ({ pkgs, ... }: pkgs.nixfmt);
+    };
+}
